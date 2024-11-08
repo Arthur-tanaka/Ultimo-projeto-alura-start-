d4cd11247db6ca110bf0288d427dff9c8bfd8193
Feragamerbox <feragamerbox@gmail.com>
2024-11-08 12:35:14 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'Python é uma linguagem de programação'
+)
+criaCartao(
+    'Biologia',
+    'O que é mitóse?',
+    'O elemento que divide a célula pra reprodução'
+)
+criaCartao(
+    'Filosofia',
+    'Qual o período de Platão?',
+    'Período da Grécia antiga'
+)
+
+criaCartao(
+    'Inglês',
+    'Qual a tradução de Lend',
+    'A tradução de Lend é emprestar'
+)
